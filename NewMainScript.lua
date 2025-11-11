e5a495059a901cb99575aab4b75c7bdcbb0ac984
wrealaero <clanwarobi@gmail.com>
2025-11-10 20:24:16 -0600

    Update NewMainScript.lua

--- NewMainScript.lua
+++ NewMainScript.lua
@@ -1,36 +1,18 @@
-local PlayerGui = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+local Player = game.Players.LocalPlayer
+local PlayerGui = Player:WaitForChild("PlayerGui")
 
 local ScreenGui = Instance.new("ScreenGui")
-ScreenGui.Name = "PermanentGUI"
-ScreenGui.ResetOnSpawn = false
+ScreenGui.Name = "FuckYouKiwi"
 ScreenGui.IgnoreGuiInset = true
+ScreenGui.ResetOnSpawn = false
 
 local TextLabel = Instance.new("TextLabel")
 TextLabel.Size = UDim2.new(1, 0, 1, 0)
 TextLabel.BackgroundColor3 = Color3.new(0, 0, 0)
-TextLabel.Text = "FUCK U KIWI HAHAHAHAH"
+TextLabel.Text = "fuck u kiwi hahahahah"
 TextLabel.TextColor3 = Color3.new(1, 1, 1)
 TextLabel.TextScaled = true
-TextLabel.Font = Enum.Font.GothamBlack
-TextLabel.TextStrokeTransparency = 0
-TextLabel.ZIndex = 10
+TextLabel.Font = Enum.Font.SourceSansBold
 TextLabel.Parent = ScreenGui
-
-local colors = {
-    Color3.new(1, 0, 0),
-    Color3.new(1, 0.5, 0),
-    Color3.new(1, 1, 0),
-    Color3.new(0, 1, 0),
-    Color3.new(0, 0, 1),
-    Color3.new(0.3, 0, 0.5),
-    Color3.new(0.6, 0, 0.8)
-}
-
-local current = 1
-while true do
-    TextLabel.TextColor3 = colors[current]
-    current = current % #colors + 1
-    wait(0.3)
-end
 
 ScreenGui.Parent = PlayerGui
