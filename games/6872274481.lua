2446a55e780b6967378836e5b57d2b70a7191858
wrealaero <clanwarobi@gmail.com>
2025-11-20 19:44:50 -0600

    FIX

--- games/6872274481.lua
+++ games/6872274481.lua
@@ -40,7 +40,6 @@ local uipallet = vape.Libraries.uipallet
 local tween = vape.Libraries.tween
 local color = vape.Libraries.color
 local whitelist = vape.Libraries.whitelist
-local prediction = vape.Libraries.prediction
 local getfontsize = vape.Libraries.getfontsize
 local getcustomasset = vape.Libraries.getcustomasset
 
@@ -2997,9 +2996,9 @@ run(function()
 								local maxAngle = math.rad(AngleSlider.Value)
 								local canSwing = delta.Magnitude <= SwingRange.Value  
 								local canHit = delta.Magnitude <= AttackRange.Value   
-								
+
 								if angle <= (maxAngle / 2) and canSwing then
-									table.insert(validTargets, v)
+									table.insert(validTargets, {entity = v, canHit = canHit})
 									hasValidTarget = true
 								end
 							end
@@ -3015,12 +3014,14 @@ run(function()
 						end
 						
 						if #validTargets > 0 then
-							for _, v in validTargets do
+							for _, targetData in validTargets do
+								local v = targetData.entity
+								local canHit = targetData.canHit
 								local delta = (v.RootPart.Position - selfpos)
 
 								table.insert(attacked, {
 									Entity = v,
-									Check = delta.Magnitude > AttackRange.Value and BoxSwingColor or BoxAttackColor
+									Check = canHit and BoxAttackColor or BoxSwingColor
 								})
 								targetinfo.Targets[v] = tick() + 1
 								pcall(function()
@@ -3091,32 +3092,34 @@ run(function()
 										end
 									end
 
-									if isClaw then
-										KaidaController:request(v.Character)
-									else
-										local attackData = {
-											weapon = sword.tool,
-											entityInstance = v.Character,
-											chargedAttack = {chargeRatio = 0},
-											validate = {
-												raycast = {
-													cameraPosition = {value = pos},
-													cursorDirection = {value = dir}
-												},
-												targetPosition = {value = targetPos},
-												selfPosition = {value = pos}
+									if canHit then 
+										if isClaw then
+											KaidaController:request(v.Character)
+										else
+											local attackData = {
+												weapon = sword.tool,
+												entityInstance = v.Character,
+												chargedAttack = {chargeRatio = 0},
+												validate = {
+													raycast = {
+														cameraPosition = {value = pos},
+														cursorDirection = {value = dir}
+													},
+													targetPosition = {value = targetPos},
+													selfPosition = {value = pos}
+												}
 											}
-										}
-										
-										attackData.validate = attackData.validate or {}
-										attackData.validate.raycast = attackData.validate.raycast or {}
-										attackData.validate.targetPosition = attackData.validate.targetPosition or {value = targetPos}
-										attackData.validate.selfPosition = attackData.validate.selfPosition or {value = pos}
-										
-										attackData.validate.raycast.cameraPosition = attackData.validate.raycast.cameraPosition or {value = pos}
-										attackData.validate.raycast.cursorDirection = attackData.validate.raycast.cursorDirection or {value = dir}
-										
-										FireAttackRemote(attackData)
+											
+											attackData.validate = attackData.validate or {}
+											attackData.validate.raycast = attackData.validate.raycast or {}
+											attackData.validate.targetPosition = attackData.validate.targetPosition or {value = targetPos}
+											attackData.validate.selfPosition = attackData.validate.selfPosition or {value = pos}
+											
+											attackData.validate.raycast.cameraPosition = attackData.validate.raycast.cameraPosition or {value = pos}
+											attackData.validate.raycast.cursorDirection = attackData.validate.raycast.cursorDirection or {value = dir}
+											
+											FireAttackRemote(attackData)
+										end
 									end
 								end
 							end
@@ -4416,6 +4419,314 @@ run(function()
 	local OtherProjectiles
 	local Blacklist
 	local TargetVisualiser
+
+	local prediction = {
+		SolveTrajectory = function(origin, projectileSpeed, gravity, targetPos, targetVelocity, playerGravity, playerHeight, playerJump, params)
+			local eps = 1e-9
+			
+			local function isZero(d)
+				return (d > -eps and d < eps)
+			end
+
+			local function cuberoot(x)
+				return (x > 0) and math.pow(x, (1 / 3)) or -math.pow(math.abs(x), (1 / 3))
+			end
+
+			local function solveQuadric(c0, c1, c2)
+				local s0, s1
+				local p, q, D
+				p = c1 / (2 * c0)
+				q = c2 / c0
+				D = p * p - q
+
+				if isZero(D) then
+					s0 = -p
+					return s0
+				elseif (D < 0) then
+					return
+				else
+					local sqrt_D = math.sqrt(D)
+					s0 = sqrt_D - p
+					s1 = -sqrt_D - p
+					return s0, s1
+				end
+			end
+
+			local function solveCubic(c0, c1, c2, c3)
+				local s0, s1, s2
+				local num, sub
+				local A, B, C
+				local sq_A, p, q
+				local cb_p, D
+
+				if c0 == 0 then
+					return solveQuadric(c1, c2, c3)
+				end
+
+				A = c1 / c0
+				B = c2 / c0
+				C = c3 / c0
+				sq_A = A * A
+				p = (1 / 3) * (-(1 / 3) * sq_A + B)
+				q = 0.5 * ((2 / 27) * A * sq_A - (1 / 3) * A * B + C)
+				cb_p = p * p * p
+				D = q * q + cb_p
+
+				if isZero(D) then
+					if isZero(q) then
+						s0 = 0
+						num = 1
+					else
+						local u = cuberoot(-q)
+						s0 = 2 * u
+						s1 = -u
+						num = 2
+					end
+				elseif (D < 0) then
+					local phi = (1 / 3) * math.acos(-q / math.sqrt(-cb_p))
+					local t = 2 * math.sqrt(-p)
+					s0 = t * math.cos(phi)
+					s1 = -t * math.cos(phi + math.pi / 3)
+					s2 = -t * math.cos(phi - math.pi / 3)
+					num = 3
+				else
+					local sqrt_D = math.sqrt(D)
+					local u = cuberoot(sqrt_D - q)
+					local v = -cuberoot(sqrt_D + q)
+					s0 = u + v
+					num = 1
+				end
+
+				sub = (1 / 3) * A
+				if (num > 0) then s0 = s0 - sub end
+				if (num > 1) then s1 = s1 - sub end
+				if (num > 2) then s2 = s2 - sub end
+
+				return s0, s1, s2
+			end
+
+			local function solveQuartic(c0, c1, c2, c3, c4)
+				local s0, s1, s2, s3
+				local coeffs = {}
+				local z, u, v, sub
+				local A, B, C, D
+				local sq_A, p, q, r
+				local num
+
+				A = c1 / c0
+				B = c2 / c0
+				C = c3 / c0
+				D = c4 / c0
+
+				sq_A = A * A
+				p = -0.375 * sq_A + B
+				q = 0.125 * sq_A * A - 0.5 * A * B + C
+				r = -(3 / 256) * sq_A * sq_A + 0.0625 * sq_A * B - 0.25 * A * C + D
+
+				if isZero(r) then
+					coeffs[3] = q
+					coeffs[2] = p
+					coeffs[1] = 0
+					coeffs[0] = 1
+
+					local results = {solveCubic(coeffs[0], coeffs[1], coeffs[2], coeffs[3])}
+					num = #results
+					s0, s1, s2 = results[1], results[2], results[3]
+				else
+					coeffs[3] = 0.5 * r * p - 0.125 * q * q
+					coeffs[2] = -r
+					coeffs[1] = -0.5 * p
+					coeffs[0] = 1
+
+					s0, s1, s2 = solveCubic(coeffs[0], coeffs[1], coeffs[2], coeffs[3])
+					z = s0
+
+					u = z * z - r
+					v = 2 * z - p
+
+					if isZero(u) then
+						u = 0
+					elseif (u > 0) then
+						u = math.sqrt(u)
+					else
+						return
+					end
+					if isZero(v) then
+						v = 0
+					elseif (v > 0) then
+						v = math.sqrt(v)
+					else
+						return
+					end
+
+					coeffs[2] = z - u
+					coeffs[1] = q < 0 and -v or v
+					coeffs[0] = 1
+
+					local results = {solveQuadric(coeffs[0], coeffs[1], coeffs[2])}
+					num = #results
+					s0, s1 = results[1], results[2]
+
+					coeffs[2] = z + u
+					coeffs[1] = q < 0 and v or -v
+					coeffs[0] = 1
+
+					if (num == 0) then
+						local results2 = {solveQuadric(coeffs[0], coeffs[1], coeffs[2])}
+						num = num + #results2
+						s0, s1 = results2[1], results2[2]
+					end
+					if (num == 1) then
+						local results2 = {solveQuadric(coeffs[0], coeffs[1], coeffs[2])}
+						num = num + #results2
+						s1, s2 = results2[1], results2[2]
+					end
+					if (num == 2) then
+						local results2 = {solveQuadric(coeffs[0], coeffs[1], coeffs[2])}
+						num = num + #results2
+						s2, s3 = results2[1], results2[2]
+					end
+				end
+
+				sub = 0.25 * A
+				if (num > 0) then s0 = s0 - sub end
+				if (num > 1) then s1 = s1 - sub end
+				if (num > 2) then s2 = s2 - sub end
+				if (num > 3) then s3 = s3 - sub end
+
+				return {s3, s2, s1, s0}
+			end
+
+			local disp = targetPos - origin
+			local p, q, r = targetVelocity.X, targetVelocity.Y, targetVelocity.Z
+			local h, j, k = disp.X, disp.Y, disp.Z
+			local l = -.5 * gravity
+
+			if math.abs(q) > 0.01 and playerGravity and playerGravity > 0 then
+				local estTime = (disp.Magnitude / projectileSpeed)
+				local origq = q
+				for i = 1, 100 do
+					q = origq - (.5 * playerGravity) * estTime
+					local velo = targetVelocity * 0.016
+					local ray = workspace:Raycast(Vector3.new(targetPos.X, targetPos.Y, targetPos.Z), 
+						Vector3.new(velo.X, (q * estTime) - playerHeight, velo.Z), params)
+					
+					if ray then
+						local newTarget = ray.Position + Vector3.new(0, playerHeight, 0)
+						estTime = estTime - math.sqrt(((targetPos - newTarget).Magnitude * 2) / playerGravity)
+						targetPos = newTarget
+						j = (targetPos - origin).Y
+						q = 0
+						break
+					else
+						break
+					end
+				end
+			end
+
+			local solutions = solveQuartic(
+				l*l,
+				-2*q*l,
+				q*q - 2*j*l - projectileSpeed*projectileSpeed + p*p + r*r,
+				2*j*q + 2*h*p + 2*k*r,
+				j*j + h*h + k*k
+			)
+			
+			if solutions then
+				local posRoots = {}
+				for _, v in solutions do
+					if v > 0 then
+						table.insert(posRoots, v)
+					end
+				end
+				posRoots[1] = posRoots[1]
+
+				if posRoots[1] then
+					local t = posRoots[1]
+					local d = (h + p*t)/t
+					local e = (j + q*t - l*t*t)/t
+					local f = (k + r*t)/t
+					return origin + Vector3.new(d, e, f)
+				end
+			elseif gravity == 0 then
+				local t = (disp.Magnitude / projectileSpeed)
+				local d = (h + p*t)/t
+				local e = (j + q*t - l*t*t)/t
+				local f = (k + r*t)/t
+				return origin + Vector3.new(d, e, f)
+			end
+		end,
+
+		predictStrafingMovement = function(targetPlayer, targetPart, projSpeed, gravity, origin)
+			if not targetPlayer or not targetPlayer.Character or not targetPart then 
+				return targetPart and targetPart.Position or Vector3.zero
+			end
+			
+			local currentPos = targetPart.Position
+			local currentVel = targetPart.Velocity
+			local distance = (currentPos - origin).Magnitude
+			
+			local baseTimeToTarget = distance / projSpeed
+			local velocityMagnitude = Vector3.new(currentVel.X, 0, currentVel.Z).Magnitude
+			local verticalVel = currentVel.Y
+			
+			local timeMultiplier = 1.0
+			if distance > 80 then
+				timeMultiplier = 0.95
+			elseif distance > 50 then
+				timeMultiplier = 0.98
+			elseif distance < 20 then
+				timeMultiplier = 1.08
+			end
+			
+			local timeToTarget = baseTimeToTarget * timeMultiplier
+			
+			local horizontalPredictionStrength = 0.80
+			if distance > 70 then
+				horizontalPredictionStrength = 0.70
+			elseif distance > 40 then
+				horizontalPredictionStrength = 0.75
+			elseif distance < 25 then
+				horizontalPredictionStrength = 0.88
+			end
+			
+			local horizontalVel = Vector3.new(currentVel.X, 0, currentVel.Z)
+			local predictedHorizontal = horizontalVel * timeToTarget * horizontalPredictionStrength
+			
+			local verticalPrediction = 0
+			local isJumping = verticalVel > 10
+			local isFalling = verticalVel < -15
+			local isPeaking = math.abs(verticalVel) < 3 and verticalVel < 1
+			
+			if isFalling then
+				verticalPrediction = verticalVel * timeToTarget * 0.32
+			elseif isJumping then
+				verticalPrediction = verticalVel * timeToTarget * 0.28
+			elseif isPeaking then
+				verticalPrediction = -2 * timeToTarget
+			else
+				verticalPrediction = verticalVel * timeToTarget * 0.25
+			end
+			
+			local finalPosition = currentPos + predictedHorizontal + Vector3.new(0, verticalPrediction, 0)
+			
+			return finalPosition
+		end,
+
+		smoothAim = function(currentCFrame, targetPosition, distance)
+			local smoothnessFactor = 0.85
+			
+			if distance > 70 then
+				smoothnessFactor = 0.75
+			elseif distance > 40 then
+				smoothnessFactor = 0.80
+			elseif distance < 20 then
+				smoothnessFactor = 0.92
+			end
+			
+			return currentCFrame:Lerp(CFrame.new(currentCFrame.Position, targetPosition), smoothnessFactor)
+		end
+	}
 
 	local rayCheck = RaycastParams.new()
 	rayCheck.FilterType = Enum.RaycastFilterType.Include
@@ -8527,17 +8838,36 @@ end)
 run(function()
     local AutoBuildUp
     local LimitItem
-    local adjacent = {}
+    local facesOnly = {
+        Vector3.new(3, 0, 0),   
+        Vector3.new(-3, 0, 0), 
+        Vector3.new(0, 3, 0),   
+        Vector3.new(0, -3, 0),  
+        Vector3.new(0, 0, 3),   
+        Vector3.new(0, 0, -3) 
+    }
     
-    for x = -3, 3, 3 do
-        for y = -3, 3, 3 do
-            for z = -3, 3, 3 do
-                local vec = Vector3.new(x, y, z)
-                if vec ~= Vector3.zero then
-                    table.insert(adjacent, vec)
-                end
+    local function checkFaceAdjacent(pos)
+        for _, v in facesOnly do
+            if getPlacedBlock(pos + v) then
+                return true
             end
         end
+        return false
+    end
+    
+    local function hasFaceBelowOrSide(pos)
+        if getPlacedBlock(pos - Vector3.new(0, 3, 0)) then
+            return true
+        end
+        
+        for _, v in facesOnly do
+            if v.Y == 0 and getPlacedBlock(pos + v) then
+                return true
+            end
+        end
+        
+        return false
     end
     
     local function nearCorner(poscheck, pos)
@@ -8549,25 +8879,22 @@ run(function()
     
     local function blockProximity(pos)
         local mag, returned = 60
-        local tab = getBlocksInPoints(bedwars.BlockController:getBlockPosition(pos - Vector3.new(21, 21, 21)), bedwars.BlockController:getBlockPosition(pos + Vector3.new(21, 21, 21)))
+        local tab = getBlocksInPoints(
+            bedwars.BlockController:getBlockPosition(pos - Vector3.new(21, 21, 21)), 
+            bedwars.BlockController:getBlockPosition(pos + Vector3.new(21, 21, 21))
+        )
+        
         for _, v in tab do
             local blockpos = nearCorner(v, pos)
             local newmag = (pos - blockpos).Magnitude
-            if newmag < mag then
+            
+            if hasFaceBelowOrSide(blockpos) and newmag < mag then
                 mag, returned = newmag, blockpos
             end
         end
+        
         table.clear(tab)
         return returned
-    end
-    
-    local function checkAdjacent(pos)
-        for _, v in adjacent do
-            if getPlacedBlock(pos + v) then
-                return true
-            end
-        end
-        return false
     end
     
     local function getScaffoldBlock()
@@ -8592,7 +8919,7 @@ run(function()
     end
     
     local function canPlaceAtPosition(blockpos)
-        if not checkAdjacent(blockpos) then
+        if not checkFaceAdjacent(blockpos) then
             return false
         end
         
@@ -8607,7 +8934,7 @@ run(function()
             checkBelow = checkBelow - Vector3.new(0, 3, 0)
         end
         
-        return hasSupport or checkAdjacent(blockpos)
+        return hasSupport or hasFaceBelowOrSide(blockpos)
     end
     
     AutoBuildUp = vape.Categories.World:CreateModule({
@@ -8628,7 +8955,7 @@ run(function()
                                 if not block then
                                     blockpos = blockpos * 3
                                     
-                                    if checkAdjacent(blockpos) then
+                                    if hasFaceBelowOrSide(blockpos) then
                                         if canPlaceAtPosition(blockpos) then
                                             task.spawn(bedwars.placeBlock, blockpos, wool, false)
                                         end
@@ -8647,7 +8974,7 @@ run(function()
                 until not AutoBuildUp.Enabled
             end
         end,
-        Tooltip = 'Automatically places blocks under you ONLY when jumping'
+        Tooltip = 'Automatically places blocks under you ONLY when jumping (no corner connections)'
     })
     
     LimitItem = AutoBuildUp:CreateToggle({
