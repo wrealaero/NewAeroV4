2e801509b2f9a6ff6eac43849bd332296d512fcb
wrealaero <clanwarobi@gmail.com>
2025-11-26 14:47:36 -0600

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,182 +1,190 @@
-repeat task.wait() until game:IsLoaded()
-if shared.vape then shared.vape:Uninject() end
 
-if identifyexecutor then
-	if table.find({'Argon', 'Wave'}, ({identifyexecutor()})[1]) then
-		getgenv().setthreadidentity = nil
-	end
+local SecurityModule = {}
+
+local function decodeBase64(data)
+    local b = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
+    data = string.gsub(data, '[^'..b..'=]', '')
+    return (data:gsub('.', function(x)
+        if (x == '=') then return '' end
+        local r,f='',(b:find(x)-1)
+        for i=6,1,-1 do r=r..(f%2^i-f%2^(i-1)>0 and '1' or '0') end
+        return r;
+    end):gsub('%d%d%d?%d?%d?%d?%d?%d?', function(x)
+        if (#x ~= 8) then return '' end
+        local c=0
+        for i=1,8 do c=c+(x:sub(i,i)=='1' and 2^(8-i) or 0) end
+        return string.char(c)
+    end))
 end
 
-local function validateSecurity()
-    local HttpService = game:GetService("HttpService")
-    
-    if not isfile('newvape/security/validated') then
-        game.StarterGui:SetCore("SendNotification", {
-            Title = "Security Error",
-            Text = "No validation file found. Access denied.",
-            Duration = 5
-        })
-        return false, nil
+local encryptedRepo = "d3JlYWxhZXJv" 
+local encryptedRepoName = "TmV3QWVyb1Y0" 
+local encryptedAccountUrl = "aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL3dyZWFsYWVyby93aGl0ZWxpc3RjaGVjay9tYWluL0FjY291bnRTeXN0ZW0ubHVh" -- Account system URL
+
+local EXPECTED_REPO_OWNER = decodeBase64(encryptedRepo)
+local EXPECTED_REPO_NAME = decodeBase64(encryptedRepoName)
+local ACCOUNT_SYSTEM_URL = decodeBase64(encryptedAccountUrl)
+
+local function createValidationFile(username, repoInfo)
+    if not isfolder('newvape/security') then
+        makefolder('newvape/security')
     end
     
-    local validationContent = readfile('newvape/security/validated')
-    local success, validationData = pcall(function()
-        return HttpService:JSONDecode(validationContent)
+    local validationData = {
+        username = username,
+        timestamp = os.time(),
+        repo_owner = repoInfo.owner,
+        repo_name = repoInfo.name,
+        validated = true,
+        checksum = game:GetService("HttpService"):GenerateGUID(false)
+    }
+    
+    local encoded = game:GetService("HttpService"):JSONEncode(validationData)
+    writefile('newvape/security/validated', encoded)
+    writefile('newvape/security/'..username, tostring(os.time()))
+end
+
+local function fetchAccounts()
+    local success, response = pcall(function()
+        return game:HttpGet(ACCOUNT_SYSTEM_URL)
     end)
-    
-    if not success or not validationData then
-        game.StarterGui:SetCore("SendNotification", {
-            Title = "Security Error",
-            Text = "Corrupted validation file. Access denied.",
-            Duration = 5
-        })
-        return false, nil
+
+    if success and response then
+        local accountsTable = loadstring(response)()
+        if accountsTable and accountsTable.Accounts then
+            return accountsTable.Accounts
+        end
     end
-    
-    if not validationData.username or not validationData.repo_owner or not validationData.repo_name or not validationData.validated then
-        game.StarterGui:SetCore("SendNotification", {
-            Title = "Security Error",
-            Text = "Invalid validation data. Access denied.",
-            Duration = 5
-        })
-        return false, nil
-    end
-    
-    if not isfile('newvape/security/'..validationData.username) then
-        game.StarterGui:SetCore("SendNotification", {
-            Title = "Security Error",
-            Text = "User validation missing. Access denied.",
-            Duration = 5
-        })
-        return false, nil
-    end
-    
-    local EXPECTED_REPO_OWNER = "wrealaero"
-    local EXPECTED_REPO_NAME = "NewAeroV4"
-    
-    if validationData.repo_owner ~= EXPECTED_REPO_OWNER or validationData.repo_name ~= EXPECTED_REPO_NAME then
-        game.StarterGui:SetCore("SendNotification", {
-            Title = "Security Error",
-            Text = "Unauthorized repository detected. Access denied.",
-            Duration = 5
-        })
-        return false, nil
-    end
-    
-    return true, validationData.username
+    return nil
 end
 
-local securityPassed, validatedUsername = validateSecurity()
-if not securityPassed then
+local function getRepoInfo()
+    local commitUrl = 'https://github.com/'..EXPECTED_REPO_OWNER..'/'..EXPECTED_REPO_NAME
+    return {
+        owner = EXPECTED_REPO_OWNER,
+        name = EXPECTED_REPO_NAME,
+        url = commitUrl
+    }
+end
+
+local function SecurityCheck(loginData)
+    if not loginData or type(loginData) ~= "table" then
+        game.StarterGui:SetCore("SendNotification", {
+            Title = "Security Error",
+            Text = "wrong loadstring bitch. dm aero",
+            Duration = 3
+        })
+        return false
+    end
+    
+    local inputUsername = loginData.Username
+    local inputPassword = loginData.Password
+    
+    if not inputUsername or not inputPassword then
+        game.StarterGui:SetCore("SendNotification", {
+            Title = "Security Error", 
+            Text = "missing yo credentials fuck u doing? dm aero",
+            Duration = 3
+        })
+        return false
+    end
+    
+    local accounts = fetchAccounts()
+    if not accounts then
+        game.StarterGui:SetCore("SendNotification", {
+            Title = "Connection Error",
+            Text = "failed to check if its yo account check your wifi it might be shitty. dm aero",
+            Duration = 3
+        })
+        return false
+    end
+    
+    local accountFound = false
+    for _, account in pairs(accounts) do
+        if account.Username == inputUsername and account.Password == inputPassword then
+            accountFound = true
+            break
+        end
+    end
+    
+    if not accountFound then
+        game.StarterGui:SetCore("SendNotification", {
+            Title = "Access Denied",
+            Text = "wrong info dm 5qvx for access",
+            Duration = 3
+        })
+        return false
+    end
+    
+    local repoInfo = getRepoInfo()
+    createValidationFile(inputUsername, repoInfo)
+    
+    return true
+end
+
+local passedArgs = ... or {}
+
+if not SecurityCheck(passedArgs) then
     return
 end
 
-shared.ValidatedUsername = validatedUsername
-
-local vape
-local loadstring = function(...)
-	local res, err = loadstring(...)
-	if err and vape then
-		vape:CreateNotification('Vape', 'Failed to load : '..err, 30, 'alert')
-	end
-	return res
-end
-local queue_on_teleport = queue_on_teleport or function() end
 local isfile = isfile or function(file)
-	local suc, res = pcall(function()
-		return readfile(file)
-	end)
-	return suc and res ~= nil and res ~= ''
+    local suc, res = pcall(function()
+        return readfile(file)
+    end)
+    return suc and res ~= nil and res ~= ''
 end
-local cloneref = cloneref or function(obj)
-	return obj
+
+local delfile = delfile or function(file)
+    writefile(file, '')
 end
-local playersService = cloneref(game:GetService('Players'))
 
 local function downloadFile(path, func)
-	if not isfile(path) then
-		local suc, res = pcall(function()
-			return game:HttpGet('https://raw.githubusercontent.com/wrealaero/NewAeroV4/'..readfile('newvape/profiles/commit.txt')..'/'..select(1, path:gsub('newvape/', '')), true)
-		end)
-		if not suc or res == '404: Not Found' then
-			error(res)
-		end
-		if path:find('.lua') then
-			res = '--This watermark is used to delete the file if its cached, remove it to make the file persist after vape updates.\n'..res
-		end
-		writefile(path, res)
-	end
-	return (func or readfile)(path)
+    if not isfile(path) then
+        local suc, res = pcall(function()
+            return game:HttpGet('https://raw.githubusercontent.com/'..EXPECTED_REPO_OWNER..'/'..EXPECTED_REPO_NAME..'/'..readfile('newvape/profiles/commit.txt')..'/'..select(1, path:gsub('newvape/', '')), true)
+        end)
+        if not suc or res == '404: Not Found' then
+            error(res)
+        end
+        if path:find('.lua') then
+            res = '--This watermark is used to delete the file if its cached, remove it to make the file persist after vape updates.\n'..res
+        end
+        writefile(path, res)
+    end
+    return (func or readfile)(path)
 end
 
-local function finishLoading()
-	vape.Init = nil
-	vape:Load()
-	task.spawn(function()
-		repeat
-			vape:Save()
-			task.wait(10)
-		until not vape.Loaded
-	end)
-
-	local teleportedServers
-	vape:Clean(playersService.LocalPlayer.OnTeleport:Connect(function()
-		if (not teleportedServers) and (not shared.VapeIndependent) then
-			teleportedServers = true
-			local teleportScript = [[
-				shared.vapereload = true
-				if shared.VapeDeveloper then
-					loadstring(readfile('newvape/loader.lua'), 'loader')()
-				else
-					loadstring(game:HttpGet('https://raw.githubusercontent.com/wrealaero/NewAeroV4/'..readfile('newvape/profiles/commit.txt')..'/loader.lua', true), 'loader')()
-				end
-			]]
-			if shared.VapeDeveloper then
-				teleportScript = 'shared.VapeDeveloper = true\n'..teleportScript
-			end
-			if shared.VapeCustomProfile then
-				teleportScript = 'shared.VapeCustomProfile = "'..shared.VapeCustomProfile..'"\n'..teleportScript
-			end
-			vape:Save()
-			queue_on_teleport(teleportScript)
-		end
-	end))
-
-	if not shared.vapereload then
-		if not vape.Categories then return end
-		if vape.Categories.Main.Options['GUI bind indicator'].Enabled then
-			vape:CreateNotification('Finished Loading', 'Welcome, '..shared.ValidatedUsername..'! '..(vape.VapeButton and 'Press the button in the top right to open GUI' or 'Press '..table.concat(vape.Keybind, ' + '):upper()..' to open GUI'), 5)
-		end
-	end
+local function wipeFolder(path)
+    if not isfolder(path) then return end
+    for _, file in listfiles(path) do
+        if file:find('loader') then continue end
+        if isfile(file) and select(1, readfile(file):find('--This watermark is used to delete the file if its cached, remove it to make the file persist after vape updates.')) == 1 then
+            delfile(file)
+        end
+    end
 end
 
-if not isfile('newvape/profiles/gui.txt') then
-	writefile('newvape/profiles/gui.txt', 'new')
+for _, folder in {'newvape', 'newvape/games', 'newvape/profiles', 'newvape/assets', 'newvape/libraries', 'newvape/guis', 'newvape/security'} do
+    if not isfolder(folder) then
+        makefolder(folder)
+    end
 end
-local gui = readfile('newvape/profiles/gui.txt')
 
-if not isfolder('newvape/assets/'..gui) then
-	makefolder('newvape/assets/'..gui)
+if not shared.VapeDeveloper then
+    local _, subbed = pcall(function()
+        return game:HttpGet('https://github.com/'..EXPECTED_REPO_OWNER..'/'..EXPECTED_REPO_NAME)
+    end)
+    local commit = subbed:find('currentOid')
+    commit = commit and subbed:sub(commit + 13, commit + 52) or nil
+    commit = commit and #commit == 40 and commit or 'main'
+    if commit == 'main' or (isfile('newvape/profiles/commit.txt') and readfile('newvape/profiles/commit.txt') or '') ~= commit then
+        wipeFolder('newvape')
+        wipeFolder('newvape/games')
+        wipeFolder('newvape/guis')
+        wipeFolder('newvape/libraries')
+    end
+    writefile('newvape/profiles/commit.txt', commit)
 end
-vape = loadstring(downloadFile('newvape/guis/'..gui..'.lua'), 'gui')()
-shared.vape = vape
 
-if not shared.VapeIndependent then
-	loadstring(downloadFile('newvape/games/universal.lua'), 'universal')()
-	if isfile('newvape/games/'..game.PlaceId..'.lua') then
-		loadstring(readfile('newvape/games/'..game.PlaceId..'.lua'), tostring(game.PlaceId))(...)
-	else
-		if not shared.VapeDeveloper then
-			local suc, res = pcall(function()
-				return game:HttpGet('https://raw.githubusercontent.com/wrealaero/NewAeroV4/'..readfile('newvape/profiles/commit.txt')..'/games/'..game.PlaceId..'.lua', true)
-			end)
-			if suc and res ~= '404: Not Found' then
-				loadstring(downloadFile('newvape/games/'..game.PlaceId..'.lua'), tostring(game.PlaceId))(...)
-			end
-		end
-	end
-	finishLoading()
-else
-	vape.Init = finishLoading
-	return vape
-end
+return loadstring(downloadFile('newvape/main.lua'), 'main')()
