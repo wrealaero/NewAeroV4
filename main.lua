c60b5c3af6c840447c1a1b125a4eb3f5008043fc
wrealaero <clanwarobi@gmail.com>
2025-11-27 15:36:56 -0800

    Update main.lua

--- main.lua
+++ main.lua
@@ -2,72 +2,10 @@ repeat task.wait() until game:IsLoaded()
 if shared.vape then shared.vape:Uninject() end
 
 if identifyexecutor then
-	local executor = ({identifyexecutor()})[1]
-	if executor == 'Argon' or executor == 'Wave' then
+	if table.find({'Argon', 'Wave'}, ({identifyexecutor()})[1]) then
 		getgenv().setthreadidentity = nil
 	end
 end
-
-local function validateSecurity()
-    local HttpService = game:GetService("HttpService")
-    
-    if not isfile('newvape/security/validated') then
-        game.StarterGui:SetCore("SendNotification", {
-            Title = "Security Error",
-            Text = "can't find yo validation file.",
-            Duration = 5
-        })
-        return false, nil
-    end
-    
-    local validationContent = readfile('newvape/security/validated')
-    local success, validationData = pcall(HttpService.JSONDecode, HttpService, validationContent)
-    
-    if not success or not validationData then
-        game.StarterGui:SetCore("SendNotification", {
-            Title = "Security Error",
-            Text = "validation file corrupted.",
-            Duration = 5
-        })
-        return false, nil
-    end
-    
-    if not (validationData.username and validationData.repo_owner and validationData.repo_name and validationData.validated) then
-        game.StarterGui:SetCore("SendNotification", {
-            Title = "Security Error",
-            Text = "wronmg validation file data.",
-            Duration = 5
-        })
-        return false, nil
-    end
-    
-    if not isfile('newvape/security/'..validationData.username) then
-        game.StarterGui:SetCore("SendNotification", {
-            Title = "Security Error",
-            Text = "user validation data missing.",
-            Duration = 5
-        })
-        return false, nil
-    end
-    
-    if validationData.repo_owner ~= "wrealaero" or validationData.repo_name ~= "NewAeroV4" then
-        game.StarterGui:SetCore("SendNotification", {
-            Title = "Security Error",
-            Text = "wrong loadstring bud.",
-            Duration = 5
-        })
-        return false, nil
-    end
-    
-    return true, validationData.username
-end
-
-local securityPassed, validatedUsername = validateSecurity()
-if not securityPassed then
-    return
-end
-
-shared.ValidatedUsername = validatedUsername
 
 local vape
 local loadstring = function(...)
@@ -79,7 +17,9 @@ local loadstring = function(...)
 end
 local queue_on_teleport = queue_on_teleport or function() end
 local isfile = isfile or function(file)
-	local suc, res = pcall(readfile, file)
+	local suc, res = pcall(function()
+		return readfile(file)
+	end)
 	return suc and res ~= nil and res ~= ''
 end
 local cloneref = cloneref or function(obj)
@@ -87,12 +27,11 @@ local cloneref = cloneref or function(obj)
 end
 local playersService = cloneref(game:GetService('Players'))
 
-local commitText = readfile('newvape/profiles/commit.txt')
-
 local function downloadFile(path, func)
 	if not isfile(path) then
-		local cleanPath = select(1, path:gsub('newvape/', ''))
-		local suc, res = pcall(game.HttpGet, game, 'https://raw.githubusercontent.com/wrealaero/NewAeroV4/'..commitText..'/'..cleanPath, true)
+		local suc, res = pcall(function()
+			return game:HttpGet('https://raw.githubusercontent.com/wrealaero/NewAeroV4/'..readfile('newvape/profiles/commit.txt')..'/'..select(1, path:gsub('newvape/', '')), true)
+		end)
 		if not suc or res == '404: Not Found' then
 			error(res)
 		end
@@ -116,28 +55,31 @@ local function finishLoading()
 
 	local teleportedServers
 	vape:Clean(playersService.LocalPlayer.OnTeleport:Connect(function()
-		if teleportedServers or shared.VapeIndependent then return end
-		teleportedServers = true
-		local teleportScript = 'shared.vapereload = true\n'
-		if shared.VapeDeveloper then
-			teleportScript = teleportScript..'shared.VapeDeveloper = true\n'
+		if (not teleportedServers) and (not shared.VapeIndependent) then
+			teleportedServers = true
+			local teleportScript = [[
+				shared.vapereload = true
+				if shared.VapeDeveloper then
+					loadstring(readfile('newvape/loader.lua'), 'loader')()
+				else
+					loadstring(game:HttpGet('https://raw.githubusercontent.com/wrealaero/NewAeroV4/'..readfile('newvape/profiles/commit.txt')..'/loader.lua', true), 'loader')()
+				end
+			]]
+			if shared.VapeDeveloper then
+				teleportScript = 'shared.VapeDeveloper = true\n'..teleportScript
+			end
+			if shared.VapeCustomProfile then
+				teleportScript = 'shared.VapeCustomProfile = "'..shared.VapeCustomProfile..'"\n'..teleportScript
+			end
+			vape:Save()
+			queue_on_teleport(teleportScript)
 		end
-		if shared.VapeCustomProfile then
-			teleportScript = teleportScript..'shared.VapeCustomProfile = "'..shared.VapeCustomProfile..'"\n'
-		end
-		if shared.VapeDeveloper then
-			teleportScript = teleportScript..'loadstring(readfile(\'newvape/loader.lua\'), \'loader\')()'
-		else
-			teleportScript = teleportScript..'loadstring(game:HttpGet(\'https://raw.githubusercontent.com/wrealaero/NewAeroV4/\'..readfile(\'newvape/profiles/commit.txt\')..\'//loader.lua\', true), \'loader\')()'
-		end
-		vape:Save()
-		queue_on_teleport(teleportScript)
 	end))
 
 	if not shared.vapereload then
 		if not vape.Categories then return end
 		if vape.Categories.Main.Options['GUI bind indicator'].Enabled then
-			vape:CreateNotification('Finished Loading', 'Welcome, '..shared.ValidatedUsername..'! '..(vape.VapeButton and 'Press the button in the top right to open GUI' or 'Press '..table.concat(vape.Keybind, ' + '):upper()..' to open GUI'), 5)
+			vape:CreateNotification('Finished Loading', vape.VapeButton and 'Press the button in the top right to open GUI' or 'Press '..table.concat(vape.Keybind, ' + '):upper()..' to open GUI', 5)
 		end
 	end
 end
@@ -157,10 +99,14 @@ if not shared.VapeIndependent then
 	loadstring(downloadFile('newvape/games/universal.lua'), 'universal')()
 	if isfile('newvape/games/'..game.PlaceId..'.lua') then
 		loadstring(readfile('newvape/games/'..game.PlaceId..'.lua'), tostring(game.PlaceId))(...)
-	elseif not shared.VapeDeveloper then
-		local suc, res = pcall(game.HttpGet, game, 'https://raw.githubusercontent.com/wrealaero/NewAeroV4/'..commitText..'/games/'..game.PlaceId..'.lua', true)
-		if suc and res ~= '404: Not Found' then
-			loadstring(downloadFile('newvape/games/'..game.PlaceId..'.lua'), tostring(game.PlaceId))(...)
+	else
+		if not shared.VapeDeveloper then
+			local suc, res = pcall(function()
+				return game:HttpGet('https://raw.githubusercontent.com/wrealaero/NewAeroV4/'..readfile('newvape/profiles/commit.txt')..'/games/'..game.PlaceId..'.lua', true)
+			end)
+			if suc and res ~= '404: Not Found' then
+				loadstring(downloadFile('newvape/games/'..game.PlaceId..'.lua'), tostring(game.PlaceId))(...)
+			end
 		end
 	end
 	finishLoading()
