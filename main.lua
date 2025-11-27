89d5d546ba220a0e918034dc17939a42fcf7c42e
wrealaero <clanwarobi@gmail.com>
2025-11-27 14:56:18 -0800

    Update main.lua

--- main.lua
+++ main.lua
@@ -8,171 +8,6 @@ if identifyexecutor then
 	end
 end
 
-local function decodeBase64(data)
-    local b = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
-    data = string.gsub(data, '[^'..b..'=]', '')
-    return (data:gsub('.', function(x)
-        if x == '=' then return '' end
-        local r,f='',(b:find(x)-1)
-        for i=6,1,-1 do r=r..(f%2^i-f%2^(i-1)>0 and '1' or '0') end
-        return r;
-    end):gsub('%d%d%d?%d?%d?%d?%d?%d?', function(x)
-        if #x ~= 8 then return '' end
-        local c=0
-        for i=1,8 do c=c+(x:sub(i,i)=='1' and 2^(8-i) or 0) end
-        return string.char(c)
-    end))
-end
-
-local ACCOUNT_SYSTEM_URL = decodeBase64("aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL3dyZWFsYWVyby93aGl0ZWxpc3RjaGVjay9tYWluL0FjY291bnRTeXN0ZW0ubHVh")
-
-local function getHardwareId()
-    local hardwareInfo = ""
-    
-    if syn and syn.crypt then
-        hardwareInfo = syn.crypt.hash(syn.crypt.random(16))
-    elseif getexecutorname then
-        hardwareInfo = getexecutorname() .. tostring(os.clock())
-    else
-        hardwareInfo = game:GetService("RbxAnalyticsService"):GetClientId() .. tostring(tick())
-    end
-    
-    local hash = ""
-    for i = 1, 32 do
-        local byte = string.byte(hardwareInfo, (i % #hardwareInfo) + 1)
-        hash = hash .. string.format("%02x", byte)
-    end
-    
-    return hash:sub(1, 16)
-end
-
-local function fetchAccounts()
-    local success, response = pcall(game.HttpGet, game, ACCOUNT_SYSTEM_URL)
-    if success and response then
-        local accountsTable = loadstring(response)()
-        if accountsTable and accountsTable.Accounts then
-            return accountsTable.Accounts
-        end
-    end
-    return nil
-end
-
-local function setupSuspensionCheck(username)
-    local checkInterval = 30
-    
-    while task.wait(checkInterval) do
-        if not vape or not vape.Loaded then break end
-        
-        local accounts = fetchAccounts()
-        if accounts then
-            local userAccount = nil
-            for _, account in pairs(accounts) do
-                if account.Username == username then
-                    userAccount = account
-                    break
-                end
-            end
-            
-            if not userAccount or userAccount.IsActive == false then
-                game.StarterGui:SetCore("SendNotification", {
-                    Title = "Account Suspended",
-                    Text = "Your access has been revoked by admin",
-                    Duration = 5
-                })
-                
-                if shared.vape then
-                    shared.vape:Uninject()
-                end
-                break
-            end
-        end
-    end
-end
-
-local function validateSecurity()
-    local HttpService = game:GetService("HttpService")
-    
-    if not isfile('newvape/security/validated') then
-        game.StarterGui:SetCore("SendNotification", {
-            Title = "Security Error",
-            Text = "can't find yo validation file.",
-            Duration = 5
-        })
-        return false, nil
-    end
-    
-    local validationContent = readfile('newvape/security/validated')
-    local success, validationData = pcall(HttpService.JSONDecode, HttpService, validationContent)
-    
-    if not success or not validationData then
-        game.StarterGui:SetCore("SendNotification", {
-            Title = "Security Error",
-            Text = "validation file corrupted.",
-            Duration = 5
-        })
-        return false, nil
-    end
-    
-    if not (validationData.username and validationData.repo_owner and validationData.repo_name and validationData.validated and validationData.hardware_id) then
-        game.StarterGui:SetCore("SendNotification", {
-            Title = "Security Error",
-            Text = "wrong validation file data.",
-            Duration = 5
-        })
-        return false, nil
-    end
-    
-    if not isfile('newvape/security/'..validationData.username) then
-        game.StarterGui:SetCore("SendNotification", {
-            Title = "Security Error",
-            Text = "user validation data missing.",
-            Duration = 5
-        })
-        return false, nil
-    end
-    
-    if validationData.repo_owner ~= "wrealaero" or validationData.repo_name ~= "NewAeroV4" then
-        game.StarterGui:SetCore("SendNotification", {
-            Title = "Security Error",
-            Text = "wrong loadstring bud.",
-            Duration = 5
-        })
-        return false, nil
-    end
-    
-    local accounts = fetchAccounts()
-    if accounts then
-        local userAccount = nil
-        for _, account in pairs(accounts) do
-            if account.Username == validationData.username then
-                userAccount = account
-                break
-            end
-        end
-        
-        if userAccount and userAccount.HardwareId and userAccount.HardwareId ~= "" then
-            local currentHardwareId = getHardwareId()
-            if userAccount.HardwareId ~= currentHardwareId then
-                game.StarterGui:SetCore("SendNotification", {
-                    Title = "Security Error",
-                    Text = "device mismatch. use your own loadstring.",
-                    Duration = 5
-                })
-                return false, nil
-            end
-        end
-    end
-    
-    return true, validationData.username
-end
-
-local securityPassed, validatedUsername = validateSecurity()
-if not securityPassed then
-    return
-end
-
-shared.ValidatedUsername = validatedUsername
-
 local vape
 local loadstring = function(...)
 	local res, err = loadstring(...)
@@ -213,10 +48,6 @@ local function finishLoading()
 	vape:Load()
 	
 	task.spawn(function()
-		setupSuspensionCheck(validatedUsername)
-	end)
-	
-	task.spawn(function()
 		repeat
 			vape:Save()
 			task.wait(10)
@@ -246,7 +77,7 @@ local function finishLoading()
 	if not shared.vapereload then
 		if not vape.Categories then return end
 		if vape.Categories.Main.Options['GUI bind indicator'].Enabled then
-			vape:CreateNotification('Finished Loading', 'Welcome, '..shared.ValidatedUsername..'! '..(vape.VapeButton and 'Press the button in the top right to open GUI' or 'Press '..table.concat(vape.Keybind, ' + '):upper()..' to open GUI'), 5)
+			vape:CreateNotification('Finished Loading', 'Welcome! Press the button in the top right to open GUI or Press '..table.concat(vape.Keybind, ' + '):upper()..' to open GUI', 5)
 		end
 	end
 end
