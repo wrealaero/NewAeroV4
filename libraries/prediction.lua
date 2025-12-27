12cf5198ac9f6e81f83c59d7e9b89422b15b07cc
wrealaero <clanwarobi@gmail.com>
2025-12-26 18:59:46 -0600

    Update prediction.lua

--- libraries/prediction.lua
+++ libraries/prediction.lua
@@ -1,6 +1,9 @@
+--[[
+	Prediction Library
+	Source: https://devforum.roblox.com/t/predict-projectile-ballistics-including-gravity-and-motion/1842434
+]]
 local module = {}
 local eps = 1e-9
-
 local function isZero(d)
 	return (d > -eps and d < eps)
 end
@@ -11,6 +14,7 @@ end
 
 local function solveQuadric(c0, c1, c2)
 	local s0, s1
+
 	local p, q, D
 
 	p = c1 / (2 * c0)
@@ -22,8 +26,9 @@ local function solveQuadric(c0, c1, c2)
 		return s0
 	elseif (D < 0) then
 		return
-	else
+	else -- if (D > 0)
 		local sqrt_D = math.sqrt(D)
+
 		s0 = sqrt_D - p
 		s1 = -sqrt_D - p
 		return s0, s1
@@ -32,6 +37,7 @@ end
 
 local function solveCubic(c0, c1, c2, c3)
 	local s0, s1, s2
+
 	local num, sub
 	local A, B, C
 	local sq_A, p, q
@@ -40,38 +46,43 @@ local function solveCubic(c0, c1, c2, c3)
 	A = c1 / c0
 	B = c2 / c0
 	C = c3 / c0
+
 	sq_A = A * A
 	p = (1 / 3) * (-(1 / 3) * sq_A + B)
 	q = 0.5 * ((2 / 27) * A * sq_A - (1 / 3) * A * B + C)
+
 	cb_p = p * p * p
 	D = q * q + cb_p
 
 	if isZero(D) then
-		if isZero(q) then
+		if isZero(q) then -- one triple solution
 			s0 = 0
 			num = 1
-		else
+		else -- one single and one double solution
 			local u = cuberoot(-q)
 			s0 = 2 * u
 			s1 = -u
 			num = 2
 		end
-	elseif (D < 0) then
+	elseif (D < 0) then -- Casus irreducibilis: three real solutions
 		local phi = (1 / 3) * math.acos(-q / math.sqrt(-cb_p))
 		local t = 2 * math.sqrt(-p)
+
 		s0 = t * math.cos(phi)
 		s1 = -t * math.cos(phi + math.pi / 3)
 		s2 = -t * math.cos(phi - math.pi / 3)
 		num = 3
-	else
+	else -- one real solution
 		local sqrt_D = math.sqrt(D)
 		local u = cuberoot(sqrt_D - q)
 		local v = -cuberoot(sqrt_D + q)
+
 		s0 = u + v
 		num = 1
 	end
 
 	sub = (1 / 3) * A
+
 	if (num > 0) then s0 = s0 - sub end
 	if (num > 1) then s1 = s1 - sub end
 	if (num > 2) then s2 = s2 - sub end
@@ -81,6 +92,7 @@ end
 
 function module.solveQuartic(c0, c1, c2, c3, c4)
 	local s0, s1, s2, s3
+
 	local coeffs = {}
 	local z, u, v, sub
 	local A, B, C, D
@@ -98,9 +110,9 @@ function module.solveQuartic(c0, c1, c2, c3, c4)
 	r = -(3 / 256) * sq_A * sq_A + 0.0625 * sq_A * B - 0.25 * A * C + D
 
 	if isZero(r) then
-		coeffs[3] = 0.5 * r * p - 0.125 * q * q
-		coeffs[2] = -r
-		coeffs[1] = -0.5 * p
+		coeffs[3] = q
+		coeffs[2] = p
+		coeffs[1] = 0
 		coeffs[0] = 1
 
 		local results = {solveCubic(coeffs[0], coeffs[1], coeffs[2], coeffs[3])}
@@ -165,6 +177,7 @@ function module.solveQuartic(c0, c1, c2, c3, c4)
 	end
 
 	sub = 0.25 * A
+
 	if (num > 0) then s0 = s0 - sub end
 	if (num > 1) then s1 = s1 - sub end
 	if (num > 2) then s2 = s2 - sub end
@@ -173,163 +186,26 @@ function module.solveQuartic(c0, c1, c2, c3, c4)
 	return {s3, s2, s1, s0}
 end
 
-local function predictMovementWithBuilding(targetPlayer, targetPart, projSpeed, gravity, origin, timeToTarget)
-	if not targetPlayer or not targetPlayer.Character or not targetPart then 
-		return targetPart and targetPart.Position or Vector3.zero
-	end
-	
-	local currentPos = targetPart.Position
-	local currentVel = targetPart.Velocity
-	
-	local isBuilding = false
-	local buildDirection = Vector3.zero
-	local buildSpeed = 0
-	
-	local nearbyBlocks = workspace:FindPartsInRegion3(
-		Region3.new(
-			currentPos - Vector3.new(5, 10, 5),
-			currentPos + Vector3.new(5, 10, 5)
-		),
-		nil,
-		50
-	)
-	
-	local upwardPlacement = false
-	for _, block in pairs(nearbyBlocks) do
-		if block.Name:find("wool") or block.Name:find("wood") or block.Name:find("stone") then
-			if block.Position.Y > currentPos.Y + 3 then
-				upwardPlacement = true
-				break
-			end
-		end
-	end
-	
-	local distance = (currentPos - origin).Magnitude
-	
-	local timeMultiplier = 1.0
-	if distance > 100 then
-		timeMultiplier = 0.92
-	elseif distance > 70 then
-		timeMultiplier = 0.95
-	elseif distance > 40 then
-		timeMultiplier = 0.98
-	elseif distance < 20 then
-		timeMultiplier = 1.05
-	elseif distance < 10 then
-		timeMultiplier = 1.10
-	end
-	
-	timeToTarget = timeToTarget or (distance / projSpeed) * timeMultiplier
-	
-	local horizontalPredictionStrength = 0.75
-	if distance > 80 then
-		horizontalPredictionStrength = 0.65
-	elseif distance > 50 then
-		horizontalPredictionStrength = 0.70
-	elseif distance > 30 then
-		horizontalPredictionStrength = 0.78
-	elseif distance < 15 then
-		horizontalPredictionStrength = 0.85
-	end
-	
-	local horizontalVel = Vector3.new(currentVel.X, 0, currentVel.Z)
-	local predictedHorizontal = horizontalVel * timeToTarget * horizontalPredictionStrength
-	
-	local verticalVel = currentVel.Y
-	local predictedVertical = 0
-	
-	local isJumping = verticalVel > 12
-	local isFalling = verticalVel < -15
-	local isPeaking = math.abs(verticalVel) < 5 and verticalVel < 2
-	local isHighJump = verticalVel > 25
-	local isLongFall = verticalVel < -30
-	
-	if upwardPlacement then
-		isBuilding = true
-		local buildVelocity = Vector3.new(0, 8, 0) 
-		predictedVertical = buildVelocity.Y * timeToTarget * 0.4
-		
-	elseif isHighJump then
-		predictedVertical = verticalVel * timeToTarget * 0.35
-		
-	elseif isLongFall then
-		predictedVertical = verticalVel * timeToTarget * 0.28
-		
-	elseif isFalling then
-		predictedVertical = verticalVel * timeToTarget * 0.32
-		
-	elseif isJumping then
-		predictedVertical = verticalVel * timeToTarget * 0.30
-		
-	elseif isPeaking then
-		predictedVertical = -3.5 * timeToTarget
-		
-	else
-		predictedVertical = verticalVel * timeToTarget * 0.25
-	end
-	
-	local heightOffset = 0
-	if distance < 20 then
-		heightOffset = 1.5 
-	elseif distance < 50 then
-		heightOffset = 1.0 
-		heightOffset = 0.5 
-	end
-	
-	local basePrediction = currentPos + predictedHorizontal
-	
-	local verticalOffset = Vector3.new(0, predictedVertical + heightOffset, 0)
-	
-	local finalPosition = basePrediction + verticalOffset
-	
-	local raycastParams = RaycastParams.new()
-	raycastParams.FilterType = Enum.RaycastFilterType.Blacklist
-	raycastParams.FilterDescendantsInstances = {targetPlayer.Character}
-	
-	local groundRay = workspace:Raycast(finalPosition, Vector3.new(0, -10, 0), raycastParams)
-	if groundRay then
-		finalPosition = Vector3.new(
-			finalPosition.X,
-			math.max(finalPosition.Y, groundRay.Position.Y + 2),
-			finalPosition.Z
-		)
-	end
-	
-	return finalPosition
-end
-
 function module.SolveTrajectory(origin, projectileSpeed, gravity, targetPos, targetVelocity, playerGravity, playerHeight, playerJump, params)
 	local disp = targetPos - origin
 	local p, q, r = targetVelocity.X, targetVelocity.Y, targetVelocity.Z
 	local h, j, k = disp.X, disp.Y, disp.Z
 	local l = -.5 * gravity
-
-	if playerGravity and playerGravity > 0 then
+	--attemped gravity calculation, may return to it in the future.
+	if math.abs(q) > 0.01 and playerGravity and playerGravity > 0 then
 		local estTime = (disp.Magnitude / projectileSpeed)
 		local origq = q
-		
-		if playerJump then
-			q = q + playerJump
-		end
-		
-		for i = 1, 50 do
-			q = origq - (.5 * playerGravity) * estTime
-			local velo = targetVelocity * 0.033
-			local ray = workspace:Raycast(
-				Vector3.new(targetPos.X, targetPos.Y, targetPos.Z), 
-				Vector3.new(velo.X, (q * estTime) - playerHeight, velo.Z), 
-				params
-			)
-			
+		local origj = j
+		for i = 1, 100 do
+			q -= (.5 * playerGravity) * estTime
+			local velo = targetVelocity * 0.016
+			local ray = workspace.Raycast(workspace, Vector3.new(targetPos.X, targetPos.Y, targetPos.Z), Vector3.new(velo.X, (q * estTime) - playerHeight, velo.Z), params)
 			if ray then
-				local newTarget = ray.Position + Vector3.new(0, playerHeight + 0.5, 0)
-				local heightDiff = math.abs(targetPos.Y - newTarget.Y)
-				if heightDiff > 5 then
-					estTime = estTime - math.sqrt((heightDiff * 2) / playerGravity)
-				end
+				local newTarget = ray.Position + Vector3.new(0, playerHeight, 0)
+				estTime -= math.sqrt(((targetPos - newTarget).Magnitude * 2) / playerGravity)
 				targetPos = newTarget
 				j = (targetPos - origin).Y
-				q = q * 0.5
+				q = 0
 				break
 			else
 				break
@@ -344,16 +220,14 @@ function module.SolveTrajectory(origin, projectileSpeed, gravity, targetPos, tar
 		2*j*q + 2*h*p + 2*k*r,
 		j*j + h*h + k*k
 	)
-	
 	if solutions then
-		local posRoots = {}
-		for _, v in solutions do
-			if v > 0 and v < 10 then 
+		local posRoots = table.create(2)
+		for _, v in solutions do --filter out the negative roots
+			if v > 0 then
 				table.insert(posRoots, v)
 			end
 		end
-		table.sort(posRoots)
-
+		posRoots[1] = posRoots[1]
 		if posRoots[1] then
 			local t = posRoots[1]
 			local d = (h + p*t)/t
@@ -369,7 +243,5 @@ function module.SolveTrajectory(origin, projectileSpeed, gravity, targetPos, tar
 		return origin + Vector3.new(d, e, f)
 	end
 end
-
-module.predictStrafingMovement = predictMovementWithBuilding
 
 return module
